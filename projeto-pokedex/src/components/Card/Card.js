207bb67a998bf72264c6533840afccef3c150a1b
Renan Lage <renanlopresti@gmail.com>
2022-01-05 18:15:12 -0300

    logica do detalhes

--- projeto-pokedex/src/components/Card/Card.js
+++ projeto-pokedex/src/components/Card/Card.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { Box, Button } from '@mui/material';
 import { CardService, Buttons, ImgCard } from './styled';
 import { goToDetails } from '../../router/coordinator'
@@ -8,61 +8,61 @@ import axios from "axios";
 import { GlobalContext } from "../../context/GlobalContext";
 
 const Card = () => {
-    const history = useHistory()
-    const { pokemons, setPokemons } = useContext(GlobalContext);
+ const history = useHistory()
+ const { states, setters } = useContext(GlobalContext);
 
-    useEffect(() => {
-        axios.get(BASE_URL)
-            .then((response) => {
-                response.data.results.map((result) => {
-                    return axios.get(result.url)
-                        .then((aux) => {
+ useEffect(() => {
+  axios.get(BASE_URL)
+   .then((response) => {
+    response.data.results.map((result) => {
+     return axios.get(result.url)
+      .then((aux) => {
 
-                            setPokemons(oldList => [...oldList, {
-                                name: result.name,
-                                url: result.url,
-                                imagem: aux.data.sprites.front_default
-                            }]);
+       setters.setPokemons(oldList => [...oldList, {
+        name: result.name,
+        url: result.url,
+        imagem: aux.data.sprites.front_default
+       }]);
 
-                        }).catch((error) => {
-                            console.log(error)
-                        })
-                });
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }, []);
+      }).catch((error) => {
+       console.log(error)
+      })
+    });
+   })
+   .catch((error) => {
+    console.log(error)
+   })
+ }, []);
 
-    return (
-        <Box
-            sx={{
-                display: 'grid',
-                gridTemplateColumns: 'repeat(6, 1fr)',
-                rowGap: 1,
-                columnGap: 2
+ return (
+  <Box
+   sx={{
+    display: 'grid',
+    gridTemplateColumns: 'repeat(6, 1fr)',
+    rowGap: 1,
+    columnGap: 2
 
-            }}>
+   }}>
 
-            {pokemons.map((pokemom) => (
-                <CardService key={pokemom.url}
-                    sx={{
-                        gridColumn: { xs: 'span 6', sm: 'span 2', md: 'span 2', lg: 'span 2', xl: 'span 1' },
-                    }}>
+   {states.pokemons.map((pokemom) => (
+    <CardService key={pokemom.url}
+     sx={{
+      gridColumn: { xs: 'span 6', sm: 'span 2', md: 'span 2', lg: 'span 2', xl: 'span 1' },
+     }}>
 
-                    <ImgCard>
-                        <img src={pokemom.imagem} alt={pokemom.name} />
-                    </ImgCard>
-                    <Buttons>
-                        <Button variant="text">Adicionar a Pokédex</Button>
-                        <Button variant="text" onClick={() => goToDetails(history)}>Ver detalhes</Button>
-                    </Buttons>
+     <ImgCard>
+      <img src={pokemom.imagem} alt={pokemom.name} />
+     </ImgCard>
+     <Buttons>
+      <Button variant="text">Adicionar a Pokédex</Button>
+      <Button variant="text" onClick={() => goToDetails(history) || setters.setPokemonDetail(pokemom.url)}>Ver detalhes</Button>
+     </Buttons>
 
-                </CardService>
-            ))}
+    </CardService>
+   ))}
 
-        </Box>
+  </Box>
 
-    )
+ )
 }
 export default Card;
